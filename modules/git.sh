c228ffb36b189d928d014e1b08f0762654663367
Calvin Yu <calvin@menue.com>
2011-03-30 10:17:54 -0400

    setup autocomplete for 'g' alias

--- modules/git.sh
+++ modules/git.sh
@@ -1,5 +1,7 @@
 # SHOW GIT INFO IN PROMPT
 source /usr/local/etc/bash_completion.d/git-completion.bash
+complete -o bashdefault -o default -o nospace -F _git g 2>/dev/null \
+  || complete -o default -o nospace -F _git g
 export PS1='\W\[\033[32m\]$(__git_ps1) \[\033[0m\]$ '
 
 alias g='git'
