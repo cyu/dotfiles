dbf8859303848b1e21ebc9665f3f99a9f57a41a6
Calvin Yu <calvin@rylabs.io>
2025-01-11 17:47:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ function doIt() {
 	source ~/.profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" || "$1" == "-f" || ! -t 0 || ! -t 1 ]]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
