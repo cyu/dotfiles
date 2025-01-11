c401c5d606fbc457b6fb2ff54a98a4c9bd6898e6
Calvin Yu <calvin@rylabs.io>
2025-01-11 17:56:02 -0500

    Update branch ref

--- install.sh
+++ install.sh
@@ -8,25 +8,14 @@ if [ -d ".git" ]; then
   git pull origin master;
 fi;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-  echo 'source ${HOME}/.bashrc_cyu' >> ~/.bashrc
-	source ~/.profile;
-}
+echo 'Installing dotfiles...';
+rsync --exclude ".git/" \
+  --exclude ".DS_Store" \
+  --exclude ".osx" \
+  --exclude "bootstrap.sh" \
+  --exclude "README.md" \
+  --exclude "LICENSE-MIT.txt" \
+  -avh --no-perms . ~;
+echo 'source ${HOME}/.bashrc_cyu' >> ~/.bashrc;
+source ~/.profile;
 
-if [[ "$1" == "--force" || "$1" == "-f" || ! -t 0 || ! -t 1 ]]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
